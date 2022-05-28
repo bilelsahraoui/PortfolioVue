8676c305573d20e7b50a9f997a275dddc5e5d762
Bilel Sahraoui <bsahraoui.mail@gmail.com>
2022-05-29 00:14:41 +0200

    Fix

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,8 +1,20 @@
 <template>
-
+  
   <router-view></router-view>
 
 </template>
 
 <script>
+import { onMounted } from "vue";
+import AOS from "aos";
+
+export default {
+  setup(){
+    onMounted(() => {
+      AOS.init();
+    })
+    return{
+    }
+  }
+}
 </script>
